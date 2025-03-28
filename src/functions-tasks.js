62a496b647c449f21d91a136368c88092e064940
zagorky <zagorkiynahyi@gmail.com>
2025-03-28 10:52:41 +0300

    feat: add 10 solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -231,8 +231,12 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let id = startFrom - 1;
+  return () => {
+    id += 1;
+    return id;
+  };
 }
 
 module.exports = {
