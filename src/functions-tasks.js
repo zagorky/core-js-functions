fbe96fba48cb61584a9ca184a6ef994aada095b5
zagorky <zagorkiynahyi@gmail.com>
2025-03-28 10:38:22 +0300

    feat: add 9 solution

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -210,8 +210,8 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => fn(...args1, ...args);
 }
 
 /**
